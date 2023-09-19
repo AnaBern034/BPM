b667f79924e6dc58eda89a7f0d82fd6743a5a45d
ana <ana.bernardes@zup.com.br>
2023-09-19 15:11:06 -0300

    commit dos desafio 1

---
+++ Desafios/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        NUMBER1.writeNumberAndReturnMaxOuMin(2,4);
+        System.out.println("===========");
+        NUMBER2.returnIfNumberIsPositiveOrNegative(-8);
+        System.out.println("===========");
+        NUMBER3.returnAletterIfVowelOrConsonante("F");
+        System.out.println("===========");
+        NUMBER4.verifyStatesOfStudents(2.5);
+        System.out.println("===========");
+        System.out.println("WRITING...");
+        NUMBER5.listOfNumber();
+    }
+}
